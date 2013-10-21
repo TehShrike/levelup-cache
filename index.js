87f684676fcdd1d5710a9bd987087c2d565e9f14
Josh Duff <me@JoshDuff.com>
2013-10-21 09:31:26 -0500

    Starting to poke at the auto-refreshing bit

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-module.exports = function turnLevelUPDatabaseIntoACache(db, getter) {
+var kind = require('kind')
+
+var StringMap = require('stringmap')
+
+module.exports = function turnLevelUPDatabaseIntoACache(db, getter, options) {
+	var refreshesSinceLastAccess = new StringMap()
+	var refreshTimeouts = new StringMap()
+
+	options = options || {}
+	options.refreshEvery = options.refreshEvery || 60 * 60 * 12
+
+	var clearRefresh = function(key) {
+		var timeout = refreshTimeouts.get(key)
+		if (typeof timeout !== 'undefined') {
+			clearTimeout(timeout)
+		}		
+	}
+
+	var resetAutoRefresh = function(key, cb) {
+		clearRefresh(key)
+		refreshTimeouts.set(key, setTimeout(cb))
+	}
+
+	var setUpAutoRefresh = function(key) {
+
+		setTimeout(function() {
+			refreshes = refreshesSinceLastAccess.get(key) || 0
+			refreshesSinceLastAccess.set(key, refreshes + 1)
+			getRemoteValue(key)
+			scheduleAutoRefresh(key)
+		}, refreshEvery * 1000)
+	}
 
 	var getRemoteValue = function(key, cb) {
+		var canCallBack = kind(cb) === 'Function'
+
 		getter(key, function(err, value) {
-			if (err) {
-				cb(err)
-			} else {
+			if (!err) {
 				db.put(key, value)
-				cb(null, value)
+				setUpAutoRefresh()
+			}
+
+			if (kind(cb) === 'Function') {
+				cb(err, value)
 			}
 		})
 	}
 
 	return function getFromCache(key, cb) {
 		db.get(key, function(err, value) {
+			refreshesSinceLastAccess.set(key, 0)
 			if (err && err.notFound) {
 				getRemoteValue(key, cb)
 			} else if (err) {
--- test/get.js
+++ test/get.js
@@ -5,30 +5,50 @@ var db = require('levelup')('/does/not/matter', {
 	db: require('memdown') 
 })
 
-var TestingCache = function(source) {
+var TestingCache = function(source, options) {
 	source = source || {
 		source1: "one",
 		source2: "two"
 	}
 
 	var getter = function(key, cb) {
-		cb(source[key])
+		console.log("getter called for " + key)
+		cb(false, source[key])
 	}
 
 	this.source = source
-	this.get = newCache(db, getter)
+	this.get = newCache(db, getter, options)
 }
 
 test("getting", function(t) {
 	t.plan(2)
 
-	cache = new TestingCache()
-	cache.get("source1", function(value) {
+	var cache = new TestingCache()
+	cache.get("source1", function(err, value) {
 		t.equal(value, "one", "The first get (requiring calling the get function) succeeds")
 
-		cache.get("source1", function(value) {
+		cache.get("source1", function(err, value) {
 			t.equal(value, "one", "The second get (returning the cached value) succeeds")
 		})
 	})
 })
 
+test("getting updated value", function(t) {
+	t.plan(3)
+	var cache = new TestingCache(false, { refreshEvery: 5 })
+
+	cache.get("source1", function(err, value) {
+		t.equal(value, "one", "The first get (requiring calling the get function) succeeds")
+		cache.source.source1 = "haha it's different now"
+
+		cache.get("source1", function(err, value) {
+			t.equal(value, "one", "The second get after the value has changed - should still return the old cached value")
+		})
+
+		setTimeout(function() {
+			cache.get("source1", function(err, value) {
+				t.equal(value, "haha it's different now", "the second get call after the remote value was changed")
+			})
+		}, 6000)
+	})
+})
