77f6c3f9c6959fe2c7af2d3e6d2ec43c174f9e40
Josh Duff <me@JoshDuff.com>
2013-12-14 23:29:22 +0000

    Fiddling with the json test

--- test/json.js
+++ test/json.js
@@ -19,47 +19,9 @@ test("encoding JSON objects", function(t) {
 		}, 5)
 	}
 
-	var cache = newCache(db, getter)
-
-	ASQ().gate(function(done) {
-		cache.get('source1', function(err, value) {
-			t.notOk(err, "no error")
-			t.ok(value, "value is truthy")
-			t.equal("hello", value.wat, "value.wat is correct")
-
-			done()
-		})
-	}, function(done) {
-		cache.get('source2', function(err, value) {
-			t.notOk(err, "no error")
-			t.ok(value, "value is truthy")
-			t.equal(value.length, 2, "value has length 2")
-			t.equal("two", value[0], "value index 0 is correct")
-			done()
-		})
-	}, function(done) {
-		cache.get('source3', function(err, value) {
-			t.notOk(err, "no error")
-			t.equal("what number am I", value, "value is correct")
-			done()
-		})
-	}, function(done) {
-		cache.get('source4', function(err, value) {
-			t.notOk(err, "no error")
-			t.ok(value, "value is truthy")
-			t.equal(1, value.length, "value has length 1")
-			t.ok(value[0].thing, "value[0].thing is truthy")
-			t.equal(value[0].thing, "stuff", "value[0].thing is correct")
-			done()
-		})
-	}).then(function(done) {
-		cache.stop()
-
-		var cache2 = newCache(db, getter)
-
-		ASQ().gate(function(done) {
-			cache2.get('source1', function(err, value) {
-				console.log(value)
+	function verify(cache, cb) {
+		return ASQ().gate(function(done) {
+			cache.get('source1', function(err, value) {
 				t.notOk(err, "no error")
 				t.ok(value, "value is truthy")
 				t.equal("hello", value.wat, "value.wat is correct")
@@ -67,7 +29,7 @@ test("encoding JSON objects", function(t) {
 				done()
 			})
 		}, function(done) {
-			cache2.get('source2', function(err, value) {
+			cache.get('source2', function(err, value) {
 				t.notOk(err, "no error")
 				t.ok(value, "value is truthy")
 				t.equal(value.length, 2, "value has length 2")
@@ -75,26 +37,35 @@ test("encoding JSON objects", function(t) {
 				done()
 			})
 		}, function(done) {
-			cache2.get('source3', function(err, value) {
+			cache.get('source3', function(err, value) {
 				t.notOk(err, "no error")
 				t.equal("what number am I", value, "value is correct")
 				done()
 			})
 		}, function(done) {
-			cache2.get('source4', function(err, value) {
+			cache.get('source4', function(err, value) {
 				t.notOk(err, "no error")
-				console.log(value)
 				t.ok(value, "value is truthy")
 				t.equal(1, value.length, "value has length 1")
 				t.ok(value[0].thing, "value[0].thing is truthy")
 				t.equal(value[0].thing, "stuff", "value[0].thing is correct")
 				done()
 			})
-		}).then(function(done) {
-			cache2.stop()
-			t.end()
-			done()
+		}).then(cb)
+	}
+
+	var cache = newCache(db, getter)
+
+	verify(cache, function() {
+		verify(cache, function() {
+			cache.stop()
+
+			var cache2 = newCache(db, getter)
+
+			verify(cache2, function() {
+				cache2.stop()
+				t.end()
+			})
 		})
-		done()
 	})
 })
